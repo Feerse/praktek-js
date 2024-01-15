9177cd0aa60d8cc4cfacb3272d7c18cb2eb9befc
Feerse <sungaikeledang6@gmail.com>
2024-01-15 11:29:35 +0800

    Initial Commit

---
+++ praktek-1/index.js
@@ -0,0 +1,15 @@
+// const root = document.getElementById('root');
+
+const namaNegara = 'Qatar';
+const benua = 'Asia';
+let populasi = 2795484;
+const kepulauan = false;
+var bahasa = 'Arab';
+
+// root.innerHTML = `<p>Nama Negara: ${namaNegara}</p>`;
+
+console.log('Nama Negara:', namaNegara);
+console.log('Benua:', benua);
+console.log('Populasi:', populasi);
+console.log('Apakah negara kepulauan?:', kepulauan);
+console.log('Bahasa:', bahasa);
