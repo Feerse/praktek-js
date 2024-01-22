029d2311fd96e6cd0f4e7f6575277e4368ad47db
Feerse <sungaikeledang6@gmail.com>
2024-01-22 14:10:19 +0800

    Modified Praktek 6

--- praktek-6/index.js
+++ praktek-6/index.js
@@ -16,3 +16,32 @@
 // let num = 42;
 // let str = String(num);
 // console.log(str);
+
+const age = 20;
+const height = 167;
+const weight = 60;
+const isStudent = true;
+
+// TODO: `age` to String
+const ageAsString = String(age);
+
+// TODO: `height` + " cm"
+const heightAsString = height + ' cm';
+
+// TODO: `weight` + " kg"
+const weightAsString = weight + ' kg';
+
+// TODO: `isStudent` to number
+const studentStatus = Number(isStudent);
+
+console.log(ageAsString);
+console.log(typeof ageAsString + '\n');
+
+console.log(heightAsString);
+console.log(typeof heightAsString + '\n');
+
+console.log(weightAsString);
+console.log(typeof weightAsString + '\n');
+
+console.log(studentStatus);
+console.log(typeof studentStatus + '\n');
